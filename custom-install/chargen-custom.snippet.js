743b07ea61f2a7d40a150858c9dc8f93b51f7982
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2026-07-18 23:36:54 -0400

    Update chargen-custom.snippet.js

--- custom-install/chargen-custom.snippet.js
+++ custom-install/chargen-custom.snippet.js
@@ -18,10 +18,10 @@
 // 3. Locate the "return {" line with field data
 // 4. Copy and paste ONLY these 4 lines into the return { } block:
 //
-//    inkling_secret_title: this.get('char.custom.inkling_secret_title'),
-//    inkling_secret_text: this.get('char.custom.inkling_secret_text'),
-//    inkling_goal_title: this.get('char.custom.inkling_goal_title'),
-//    inkling_goal_text: this.get('char.custom.inkling_goal_text'),
+    inkling_secret_title: this.get('char.custom.inkling_secret_title'),
+    inkling_secret_text: this.get('char.custom.inkling_secret_text'),
+    inkling_goal_title: this.get('char.custom.inkling_goal_title'),
+    inkling_goal_text: this.get('char.custom.inkling_goal_text'),
 //
 // 5. Save the file
 //
